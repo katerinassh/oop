f454c55d468a7895aae2dadd0e7b8cb882c0532a
katerinassh <ekaterina.shakiryanova@gmail.com>
2020-10-21 15:49:43 +0300

    costructor class Test

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("Hello")
+class Test():
+    def __init__(self, title, description, qamount, level):
+        self.title = title
+        self.decription = description
+        self.qamount = 0
+        self.level = 0
