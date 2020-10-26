e2785a428b49ebd264a453dfe33cb971e4fc452f
katerinassh <ekaterina.shakiryanova@gmail.com>
2020-10-26 16:36:16 +0200

    Разделение класса Test и Manager; инкапсуляция методов класса Test

--- test.py
+++ test.py
@@ -1,27 +1,15 @@
 class Test(): # клас менеджер-тест, взаємодія і з адміністратором, і зі звичайним користувачем
-    def __init__(self, title, description, qamount, level):
+    def __init__(self, title, description, qamount):
         self.title = title
         self.decription = description
         self.qamount = 0
-        self.level = 0
 
-    def create_newtest(self):# метод створює новий тест
+    def _add(self, type):# метод додає нове питання у тест
         return
 
-    def add(self, type):# метод додає нове питання у тест
+    def _remove(self, number):# метод видаляє певне питання з тесту
         return
 
-    def remove(self, number):# метод видаляє певне питання з тесту
+    def _edit(self, number):# метод редагує питання
         return
 
-    def edit(self, number):# метод редагує питання
-        return
-
-    def display(self):# метод виводить тест для проходження
-        return
-
-    def save_result(self):# метод збергігає результат проходження тесту
-        return
-
-    def save_test(self):# метод зберігає усі зміни адміністратором
-        return
