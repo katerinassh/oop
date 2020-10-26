4c478493e56e4da21aad9ca9a487e54a74c02b27
katerinassh <ekaterina.shakiryanova@gmail.com>
2020-10-26 18:30:04 +0200

    Разделение класса Test и Manager; инкапсуляция методов класса Test

--- 2types_of_questions.py
+++ 2types_of_questions.py
@@ -2,7 +2,6 @@ class qstTrueFalse: # клас для виду запитань із двома 
     def true_false(self):
         return bool
 
-
 class qstEnterText: # клас для виду запитань із введенням текстової відповідді
     def small_answer(self): # метод для введення відповідді з обмеженням у 50 символів
         return
