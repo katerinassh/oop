6ee8d28d7379a8c0d05fc4f480b27541f7acc25d
SulfurTech <vanoserovv@gmail.com>
2020-10-27 18:58:34 +0200

    Change class Manager

--- manager.py
+++ manager.py
@@ -1,3 +1,12 @@
-class Manager:
-    def __init__(self):
+class Manager(Test):
+    def create_newtest(self):  # метод створює новий тест
+        return
+
+    def display(self):# метод виводить тест для проходження
+        return
+
+    def save_result(self):  # метод збергігає результат проходження тесту
+        return
+
+    def save_test(self):  # метод зберігає усі зміни адміністратором
         return
