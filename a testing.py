5c81817bf3f9900f0ca87ac7e3ff406789712555
valeriiadidych <leresta1507@gmail.com>
2020-12-15 22:15:34 +0200

    видалені непотрібні методи в класі QstName

--- a testing.py
+++ a testing.py
@@ -1,10 +1,10 @@
 import test
 
 test1 = test.Test('How are you doing', 'Something about information you may know')
-#test1.createAnswerFile()
-#test1.add('QstName')
-#test1.add('QstEnterText')
-#test1.add('QstOneAnswer')
-#test1.workTestFile()
-#test1.workAnswerFile()
+test1.createAnswerFile()
+test1.add('QstName')
+test1.add('QstEnterText')
+test1.add('QstOneAnswer')
+test1.workTestFile()
+test1.workAnswerFile()
 test1.passingTest()
--- test.py
+++ test.py
@@ -28,7 +28,7 @@ class Test(): # клас менеджер-тест, взаємодія і з а�
 
     def workAnswerFile(self):  # метод записує усі відповіді певного респондента
         self.fanswers = open('{}_answers.txt'.format(self.title), "a")
-        self.fanswers.write("\n" + self.questions[0].user_answer + "\n")
+        self.fanswers.write("\n" + str(self.questions[0].user_answer) + "\n")
         self.fanswers = open('{} answers.txt'.format(self.title), "a")
         self.fanswers.write("\n" + str(self.questions[0].user_answer) + "\n")
         i = 1
