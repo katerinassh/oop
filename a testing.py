6fae81871342ce7f44d41ee9a6fded9ba31b2b03
valeriiadidych <leresta1507@gmail.com>
2020-12-18 18:57:13 +0200

    налагоджена робота класу Feedback

--- a testing.py
+++ a testing.py
@@ -1,11 +1,22 @@
 import manager
+import feedback
 
-man = manager.Manager()
-man.create_new_test()
-man.current_test.add('QstEnterText')
+#fb = feedback.Feedback("Module 1. Reading", 2)
+#fb.filter_by_mark(12, "more")
+#fb.sort_by_mark()
+#fb.sort_by_name()
+#fb.statistic_by_mark(15)
+
+
+#man = manager.Manager()
+#man.create_new_test()
+#man.current_test.add('QstEnterText')
+
 #man.current_test.add('QstTable')
-man.current_test.add('QstScale')
-man.current_test.workTestFile()
-man.current_test.passingTest()
-man.current_test.passingTest()
+
+#man.current_test.add('QstScale')
+#man.current_test.workTestFile()
+#man.current_test.passingTest()
+#man.current_test.passingTest()
+
 #man.delete()
