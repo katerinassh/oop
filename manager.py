4ca6c79c1afff0cc4ccdd05a6ff2c4a90a2cec85
SulfurTech <vanoserovv@gmail.com>
2020-12-21 19:18:06 +0200

    Add access update

--- manager.py
+++ manager.py
@@ -7,6 +7,7 @@ class Manager:
     def __init__(self):
         self.current_test = None
         self.feedback = None
+        self.access = False
 
     def create_new_test(self):  # метод створює новий тест
         print('Input name of test and description in different rows')
@@ -69,7 +70,7 @@ statistic by mark - to create new file with statistic''')
             open - to open file by input name
             new - to create new Test file
             exit - to close program
-            \t(Test need to be opened before)
+            \t(Test need to be opened or created before)
             pass - to pass current Test 
             add - to add qst to current Test
             edit - to rewrite qst with number
@@ -80,45 +81,66 @@ statistic by mark - to create new file with statistic''')
             statistic by mark - to create new file with statistic''')
                 continue
             elif command == "new":
+                self.access = False
                 self.create_new_test()
                 self.upd_feedback()
             elif command == "del":
                 self.delete()
                 continue
             elif command == "open":
+                self.access = True
                 self.open()
                 self.upd_feedback()
             elif command == "pass":
-                self.display()
+                if self.access:
+                    self.display()
+                else:
+                    print('You need to save or open Test')
             elif command == "exit":
                 break
             elif command == "add":
+                self.access = False
                 print('Input type of Qst')
                 type = input()
                 self.current_test.add(type)
             elif command == "edit":
+                self.access = False
                 print('Input number of Qst to edit')
                 num = int(input())
                 self.current_test.rewrite(num)
             elif command == "remove":
+                self.access = False
                 print('Input number of Qst to delete')
                 num = int(input())
                 self.current_test.remove(num)
             elif command == "save":
+                self.access = True
                 self.current_test.workTestFile()
                 self.upd_feedback()
             elif command == "sort by name":
-                self.feedback.sort_by_name()
+                if self.access:
+                    self.feedback.sort_by_name()
+                else:
+                    print('You need to save or open Test')
             elif command == "sort by mark":
-                self.feedback.sort_by_mark()
+                if self.access:
+                    self.feedback.sort_by_mark()
+                else:
+                    print('You need to save or open Test')
             elif command == "statistic by mark":
-                self.feedback.statistic_by_mark(self.current_test.total_mark)
+                if self.access:
+                    self.feedback.statistic_by_mark(self.current_test.total_mark)
+                else:
+                    print('You need to save or open Test')
             elif command == "filter":
-                print('Input mark')
-                limit = float(input())
-                print('Input "less" or "more"')
-                Or = input()
-                self.feedback.filter_by_mark(limit, Or)
+                if self.access:
+                    print('Input mark')
+                    limit = float(input())
+                    print('Input "less" or "more"')
+                    Or = input()
+                    self.feedback.filter_by_mark(limit, Or)
+                else:
+                    print('You need to save or open Test')
             else:
                 print('Invalid command')
                 continue
